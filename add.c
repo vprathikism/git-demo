fd0b12f5ddc393691bef52c98f389648344d8783
vprathik <vemsaiprathikreddy@gmail.com>
2024-10-16 22:26:04 +0530

    Implemented loops in addition

--- add.c
+++ add.c
@@ -1,14 +1,29 @@
+
 #include <stdio.h>
 
-int main() {
+// Function prototype
+int add(int a, int b);
 
-	int a,b;
-	int sum;
-	printf("Enter first integer");
-	scanf("%d",&a);
-	printf("Enter second integer");
-	scanf("%d",&b);
-	sum=a+b;
-	printf("The sum of %d and %d is %d",a,b,sum);
-	return 0;
+int main() {
+    int num1, num2, sum;
+
+    // User input
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Call the add function
+    sum = add(num1, num2);
+
+    // Display the result
+    printf("The sum of %d and %d is: %d\n", num1, num2, sum);
+
+    return 0;
 }
+
+// Function to add two integers
+int add(int a, int b) {
+    return a + b;
+}
+
